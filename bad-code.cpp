7c45fe457ac45e1795bdab6761e63f25517895d5
jiangr0421@gmail.com <ryanjiang@DESKTOP-109BKGG.localdomain>
2021-10-16 13:49:31 -0400

    Added bad code

--- bad-code.cpp
+++ bad-code.cpp
@@ -1,3 +1,11 @@
+/*
+Author: Ryan Jiang
+Course: CSCI-135
+Instructor: Tong Yi and Mike Zamansky
+Assignment: Lab 7
+This program contains bad code used for tests.
+*/
+
                 int main(){
            // Hi, I'm a program!
 int x = 1; 
