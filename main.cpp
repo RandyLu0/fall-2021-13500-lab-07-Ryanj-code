3ebe2334e33fad7efe8bfd4b37cf9788c23513c3
Ryan Jiang <jiangr0421@gmail.com>
2021-10-20 16:13:43 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -39,5 +39,49 @@ int main(){
 
   fin.close();
   
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  std::cout << "Bad Code 2:" << "\n";
+
+  std::ifstream fin2("bad-code2.cpp");
+  if(fin2.fail()){
+    std::cerr << "File cannot be opened";
+    exit(1);
+  }
+
+  std::string line2;
+  while(getline(fin2, line2)) {
+    std::cout << line2 << "\n";
+  }
+
+  fin2.close();
+  fin2.open("bad-code2.cpp");
+  
+  std::cout << "\n" << "Testing unindent: " << "\n";
+
+  while(getline(fin2, line2)) {
+    std::cout << removeLeadingSpaces(line2);
+  }
+  
+  fin2.close();
+  fin2.open("bad-code2.cpp");
+
+  std::cout << "\n" << "Testing indent: " << "\n";
+  
+  std::string unindent2;
+  while(getline(fin2, line2)) {
+    unindent = removeLeadingSpaces(line2);
+    std::cout << indent(unindent2) << "\n";
+  }
+
+  fin2.close();
+  
   return 0;
 }
