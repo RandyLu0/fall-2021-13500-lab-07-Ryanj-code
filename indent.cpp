3caf92b4b321b4490539b285158166ebb8809a9c
jiangr0421@gmail.com <ryanjiang@DESKTOP-109BKGG.localdomain>
2021-10-20 16:26:33 -0400

    Added comments

--- indent.cpp
+++ indent.cpp
@@ -16,11 +16,11 @@ int countChar(std::string line, char c){
   for(int i = 0; i < line.length(); i++){
     if(line[i] == c){
       count++;
-    }
-  }
+    } // Goes through line and checks if any char c occurs in line.
+  } // Everytime c occurs in line, count increments by 1.
 
   return count;
-}
+} // Return count.
 
 int count = 0;
 
@@ -29,20 +29,20 @@ std::string indent(std::string line){
   
   for(int i = 0; i < count; i++){
     s += "\t";
-  }
+  } // Adds count number of \t before the line.
 
   s += line;
-
+  // Adds the line to s;
   if(countChar(line, '{') > 0){
     count++;
-  }
+  } // Increment count by 1 if an open bracket is in the line. 
 
   if(countChar(line, '}') > 0){
     count--;
-  }
+  } // Decrement count by 1 if a closed bracket is in the line.
   
   return s;
-}
+} // Return s.
 
 
 
--- unindent.cpp
+++ unindent.cpp
@@ -17,14 +17,14 @@ std::string removeLeadingSpaces(std::string line){
   for(int i = 0; i < line.length(); i++){
     if(!isspace(line[i])){
 	check = true;
-    }
+    } // When the first character in the line that is not a space appears, set check to true;
 
     if(check){
       s += line.substr(i);
       s += "\n";
-      break;
-    }
-  }
+      break; 
+    } // If check is true, add the line and \n to s, and then break.
+  } 
 
   return s;
-}
+} // Return s.
