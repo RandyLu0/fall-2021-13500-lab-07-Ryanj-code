abe178b2138a27b97c198e69a1ddbb57007b75c6
Ryan Jiang <jiangr0421@gmail.com>
2021-10-20 16:09:03 -0400

    Create bad-code2.cpp

---
+++ bad-code2.cpp
@@ -0,0 +1,16 @@
+                int main(){
+    // Hi, I'm a program!
+                int x = 2; 
+                  for(int i = 0; i < 10; i++) {
+      if(x > 1) {
+                    x--;
+     }
+                   else if(x < 1) {
+                  x+= 2;
+  }
+                        else{
+        x+= x;
+        }
+     }
+                  cout << "X is now: " << x;
+          }
